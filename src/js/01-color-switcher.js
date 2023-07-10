04a0f7ba2993fbe8464240f19e68ca27a2bc2f81
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2023-07-10 18:44:37 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,14 +6,26 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
   }
   
+  function disableButton(button) {
+    button.disabled = true;
+  }
+  
+  function enableButton(button) {
+    button.disabled = false;
+  }
+  
   startBtn.addEventListener("click", () => {
-    timerId = setInterval(() => {
-      const randomColor = getRandomHexColor();
-      
-      document.body.style.backgroundColor = randomColor;
-    }, 1000);
+    if (!timerId) {
+      timerId = setInterval(() => {
+        const randomColor = getRandomHexColor();
+        document.body.style.backgroundColor = randomColor;
+      }, 1000);
+      disableButton(startBtn); 
+    }
   });
   
   stopBtn.addEventListener("click", () => {
     clearInterval(timerId);
+    timerId = null;
+    enableButton(startBtn); 
   });
